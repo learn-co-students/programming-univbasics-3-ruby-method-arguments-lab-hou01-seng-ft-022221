3e93e2b869cce007f5b7f4b50b7085083e073fda
Jose Mendoza <pball626@gmail.com>
2021-02-05 17:35:25 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,13 +2,17 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
+def introduction(jose)
+  puts "Hi, my name is #{jose}."
+end
 
+def introduction_with_language(jose, ruby)
+puts "Hi, my name is #{jose} and I am learning to program in #{ruby}."
+end
 
-
-
-
-
-
+def introduction_with_language_optional(jose, language = "Ruby")
+  puts "Hi, my name is #{jose} and I am learning to program in #{language}."
+end
 
 
 
